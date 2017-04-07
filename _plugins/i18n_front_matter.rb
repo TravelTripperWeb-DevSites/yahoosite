fab73b9a670b109fbb75279b8984d98dc3821c87
tt-admin <tt-admin@users.noreply.github.com>
2017-04-07 06:20:41 -0400

    Update Plugins

--- _plugins/i18n_front_matter.rb
+++
@@ -1,8 +0,0 @@
-Jekyll::Hooks.register :site, :post_read do |site|
-  locale = site.active_lang
-  default_locale = site.default_lang
-
-  site.pages.each do |page|
-    page.data = translate_data(page.data, locale, default_locale)
-  end
-end
